f6d4e9d6f1fd1a2e7af29d850d341f2afaebbb53
yhamirMaldonadoVanegas <147953338+yhamirMaldonadoVanegas@users.noreply.github.com>
2023-10-30 22:09:31 +0000

    main

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-import admin, app 
+import admin, app
 
-st.set_page_config(
-    page_title = "Menu"
-)
+class MultiApp:
+    def __init__(self):
+        self.apps = []
+    def add_app(self, title, fun): 
+        self.apps.append({
+            "title":title,
+            "function":func
+        })
+
+    def run():
+        with st.sidebar:
+            app = option_menu(
+                menu_title="Menu",
+                options=["Admin","Home"],
+                icons=["person-circle","house"],
+                menu_icon="cast",
+                default_index=1,
+            )
